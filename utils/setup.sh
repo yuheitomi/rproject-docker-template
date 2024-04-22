64a213beeecf093a93244f149ee6d73610ff33fc
Yuhei Kuratomi <yuhei.kuratomi@jgia.co.jp>
2024-04-22 13:01:42 +0900

    Update RStudio preferences and setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,3 +1,4 @@
+# Install custom fonts for RStudio
 mkdir -p ~/.config/rstudio/fonts/PlemolJPConsole/400/italic
 mkdir -p ~/.config/rstudio/fonts/PlemolJPConsole/700/italic
 wget https://github.com/yuru7/PlemolJP/releases/download/v1.7.1/PlemolJP_v1.7.1.zip
@@ -8,3 +9,6 @@ cp PlemolJP_v1.7.1/PlemolJPConsole/PlemolJPConsole-Bold.ttf ~/.config/rstudio/fo
 cp PlemolJP_v1.7.1/PlemolJPConsole/PlemolJPConsole-BoldItalic.ttf ~/.config/rstudio/fonts/PlemolJPConsole/700/italic
 rm PlemolJP_v1.7.1.zip
 rm -rf PlemolJP_v1.7.1
+
+# Copy custom RStudio configuration
+cp utils/rstudio-prefs.json ~/.config/rstudio/rstudio-prefs.json
